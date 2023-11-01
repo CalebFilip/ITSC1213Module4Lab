be514b2e0ccb344787614619e558e27c26026543
CalebFilip <144945438+CalebFilip@users.noreply.github.com>
2023-11-01 14:51:42 -0400

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Square {
+
+    private int[][] square;
+
+    //--------------------------------------
+    //create new square of given size
+    //--------------------------------------
+    public Square(int size) {
+        square = new int[size][size];
+
+    }
+    
+    //--------------------------------------
+    //create new square using given 2D array
+    //--------------------------------------
+    public Square(int[][] matrix) {
+        square = matrix;
+
+    }
+
+    //--------------------------------------
+    //return the square 2D array
+    //--------------------------------------
+    public int[][] getSquare() {
+        return square;
+    }
+
+    //--------------------------------------
+    //return the sum of the values in the given row
+    //--------------------------------------
+    public int sumRow(int row) {
+        int total = 0;
+        for (int i = 0; i < square.length; i++) {
+            total += square[row][i];
+        }
+        return total;
+    }
+
+    //--------------------------------------
+    //return the sum of the values in the given column
+    //--------------------------------------
+    public int sumColumn(int col) {
+        int total = 0;
+       for (int i = 0; i < square.length; i++){
+           total = total + square[i][col];
+        }
+        return total;
+    }
+
+    //--------------------------------------
+    //return the sum of the values in the main diagonal
+    //--------------------------------------
+    public int sumMainDiag() {
+        int total = 0;
+        for (int i = 0; i < square.length; i++){
+            total += square[i][i];
+        }
+        return total;
+    }
+
+    //--------------------------------------
+    //return the sum of the values in the other ("reverse") diagonal
+    //--------------------------------------
+    public int sumOtherDiag() {
+        int total = 0;
+        for (int i = 0; i < square[0].length; i++){
+            total += square[i][square.length - i-1];
+            
+        }
+        return total;
+
+    }
+
+    //--------------------------------------
+    //return true if the square is magic (all rows, cols, and diags have
+    //same sum), false otherwise
+    //--------------------------------------
+    public boolean isMagic() {
+         int targetSum = sumRow(0);
+        for (int i = 1; i < square.length; i++) {
+            if (sumRow(i) != targetSum) {
+                return false;
+            }
+        }
+        for (int i = 0; i < square.length; i++) {
+            if (sumColumn(i) != targetSum) {
+                return false;
+            }
+        }
+        if (sumMainDiag() != targetSum) {
+            return false;
+        }
+         if (sumOtherDiag() != targetSum) {
+            return false;
+        }
+        return true; 
+    }
+
+    //--------------------------------------
+    //read info into the square from the standard input.
+    //--------------------------------------
+    public void readSquare(Scanner scan) {
+        for (int row = 0; row < square.length; row++) {
+            for (int col = 0; col < square.length; col++) {
+                System.out.println("Enter an integer number");
+                square[row][col] = scan.nextInt();
+            }
+        }
+    }
+
+    //--------------------------------------
+    //print the contents of the square, neatly formatted
+    //--------------------------------------
+    public void printSquare() {
+        for (int row = 0; row < square.length; row++) {
+            for (int col = 0; col < square.length; col++) {
+                System.out.print(square[row][col] + "  ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
